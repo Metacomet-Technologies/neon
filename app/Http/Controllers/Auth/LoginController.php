2bcf396406cc9ee91a67f98bcb459ba17b18980e
Devon Garbalosa <dgarbalo@gmail.com>
2025-01-23 21:10:11 -0500

    update login route for ignoring inertia

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
+use Inertia\Inertia;
 use Laravel\Socialite\Facades\Socialite;
 
 final class LoginController
@@ -11,8 +13,15 @@ final class LoginController
     /**
      * Handle the incoming request.
      */
-    public function __invoke(): \Symfony\Component\HttpFoundation\RedirectResponse
+    public function __invoke(Request $request): \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response
     {
+        // Check if the request is an Inertia request
+        if ($request->header('X-Inertia')) {
+            // Return a response that instructs the client to perform a full page reload
+            return Inertia::location(route('login'));
+        }
+
+        // Handle the request normally
         return Socialite::driver('discord')->redirect();
     }
 }
