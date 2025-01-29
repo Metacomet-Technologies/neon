249f46794ba9fd31d80d6f5ff06e31a397613924
Devon Garbalosa <dgarbalo@gmail.com>
2025-01-28 21:44:04 -0500

    added heartbeat for envoyer

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Artisan::command('ping', function () {
+    $this->info('Pong');
+})->everyFiveMinutes()
+    ->thenPing('http://beats.envoyer.io/heartbeat/fsRbxXpU82GSTaF');
