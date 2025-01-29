c1d570b5fe608093d03ca6a90591a2d1bf6a5df4
Devon Garbalosa <dgarbalo@gmail.com>
2025-01-28 21:24:18 -0500

    added sentry replay

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,6 +2,7 @@ import '../css/app.css';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/react';
+import * as Sentry from '@sentry/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { StrictMode } from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
@@ -9,6 +10,19 @@ import { createRoot, hydrateRoot } from 'react-dom/client';
 import { ThemeProvider } from './Layout/ThemeContext';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_LARAVEL_DSN,
+    integrations: [
+        Sentry.replayIntegration({
+            maskAllText: false,
+            blockAllMedia: false,
+        }),
+    ],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 createInertiaApp({
     title: (title) => (title ? `${title} &middot; ${appName}` : appName),
--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -5,10 +5,24 @@ import { StrictMode } from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { RouteName } from 'ziggy-js';
 
+import * as Sentry from '@sentry/react';
 import { route } from '../../vendor/tightenco/ziggy';
 import { ThemeProvider } from './Layout/ThemeContext';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_LARAVEL_DSN,
+    integrations: [
+        Sentry.replayIntegration({
+            maskAllText: false,
+            blockAllMedia: false,
+        }),
+    ],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 createServer((page) =>
     createInertiaApp({
