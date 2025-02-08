53be86e871650863deb88f6cbdf166dfdfb040b5
Devon Garbalosa <58236685+DGarbs51@users.noreply.github.com>
2025-02-08 17:06:28 -0500

    added laravel nova

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    \SocialiteProviders\Manager\ServiceProvider::class,
+    App\Providers\NovaServiceProvider::class,
+    SocialiteProviders\Manager\ServiceProvider::class,
 ];
