3d21b24e529e67740612b9c71f619c5a6dd66184
Devon Garbalosa <devon@metacomet.tech>
2025-04-12 21:07:45 -0400

    feat: scheduled refresh of guilds the bot belongs to

---
+++ app/Helpers/Discord/GetBotGuilds.php
@@ -0,0 +1,40 @@
+<?php
+declare (strict_types = 1);
+namespace App\Helpers\Discord;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+
+class GetBotGuilds
+{
+    public static function make()
+    {
+        return (new self())();
+    }
+
+    public function __invoke()
+    {
+        $key = 'neon:guilds';
+        $ttl = 300;
+        return Cache::remember($key, $ttl, function () {
+            return $this->getGuildIds();
+        });
+
+        return $guilds;
+    }
+
+    public function getGuildsFromDiscord()
+    {
+        $response = Http::withToken(config('discord.token'), 'Bot')
+            ->baseUrl(config('services.discord.rest_api_url'))
+            ->get('/users/@me/guilds');
+
+        return $response->successful() ? $response->json() : [];
+    }
+
+    public function getGuildIds()
+    {
+        $guilds = $this->getGuildsFromDiscord();
+        return collect($guilds)->pluck('id')->all();
+    }
+}
---
+++ app/Jobs/RefreshNeonGuildsJob.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Jobs;
+
+use App\Helpers\Discord\GetBotGuilds;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Cache;
+
+class RefreshNeonGuildsJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $key = 'neon:guilds';
+        $ttl = 300;
+
+        $guilds = new GetBotGuilds();
+
+        Cache::put($key, $guilds->getGuildIds(), $ttl);
+    }
+}
