203b3a7de25d6ed9af713ac73cdcf0693a10b462
Devon Garbalosa <devon@metacomet.tech>
2025-06-19 12:00:47 -0400

    fix: remove envoyer details

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,6 @@
 <?php
 
 use App\Jobs\RefreshNeonGuildsJob;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('ping', function () {
-    $this->info('Pong');
-})->everyFiveMinutes()
-    ->thenPing('http://beats.envoyer.io/heartbeat/fsRbxXpU82GSTaF');
 
 Schedule::job(new RefreshNeonGuildsJob)->everyTwoMinutes();
